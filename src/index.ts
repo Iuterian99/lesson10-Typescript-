1fa0317197cc32e984c40950367bd4520ba940f1
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-04-11 12:55:10 +0500

    basic tsc

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
 
-app.get("./", (req: Request, res: Response) => {
-  res.send("Ok");
+const fn = (num1: number, num2: number): number => num1 + num2;
+
+app.get("/", (req: Request, res: Response) => {
+  console.log(fn(2, 3));
+
+  res.send("ok");
 });
 
 app.listen(9000, () => {
